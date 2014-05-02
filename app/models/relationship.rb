143b8e05a6359155bde117e6f78678bee2915f84
Nobigie <vdebost@gmail.com>
2014-05-02 13:24:12 +0200

    11.1 The Relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,6 @@
 class Relationship < ActiveRecord::Base
-  belongs_to :followers, class_name: "User"
-  belongs_to :followed, class_name: "User"
+  belongs_to :followers, class_name: "User" #On flag 2 types de followers
+  belongs_to :followed, class_name: "User" #On flag 2 types de followers
 
   validates :followers_id, presence: true
   validates :followed_id, presence: true
